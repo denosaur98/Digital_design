bad4ee0f3e63453fd74aff0416ce543b47f7119f
Denis <109144817+denosaur98@users.noreply.github.com>
2023-05-13 10:15:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+//Стики хэдер
 window.addEventListener('scroll', function() {
   const header = document.querySelector('header')
   header.style.transform = 'translateY(' + window.scrollY + 'px)'
 })
 
+//Скролл к нужным пунктам
 const linkRates = document.getElementById(1).addEventListener('click', scrollToBlocks)
 const linkRules = document.getElementById(2).addEventListener('click', scrollToRules)
 
@@ -24,6 +26,7 @@ function scrollToRules(event) {
   })
 }
 
+//Кнопка вверх
 const firstHeading = document.querySelector('h1')
 const scrollUpLink = document.createElement('a')
 scrollUpLink.href = '#'
@@ -45,3 +48,23 @@ scrollUpLink.addEventListener('click', (event) => {
     behavior: 'smooth'
   })
 })
+
+//Дата добавления
+const blocks = document.querySelectorAll('.block')
+blocks.forEach(block => {
+  const currentDate = getCurrentDate()
+  const dateElement = document.createElement('p')
+  dateElement.classList.add('date')
+  dateElement.textContent = `Добавлено: ${currentDate}`
+  block.appendChild(dateElement)
+});
+function getCurrentDate() {
+  const weekdays = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+  const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
+  const now = new Date()
+  const dayOfWeek = weekdays[now.getDay()]
+  const weekNumber = Math.ceil(now.getDate() / 7)
+  const monthName = months[now.getMonth()]
+  const year = now.getFullYear()
+  return `${dayOfWeek}, ${weekNumber} неделя ${monthName} ${year}г`
+}
